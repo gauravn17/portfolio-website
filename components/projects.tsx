621588a1f82b3684bb7473013bab45b863432dbc
gauravn17 <93863977+gauravn17@users.noreply.github.com>
2025-05-16 18:46:17 -0700

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -15,36 +15,37 @@ export default function Projects() {
   {
     title: "VibeMatch – ML Roommate Matcher",
     description: [
-      "Built a machine-learning-based roommate matching app using personality traits and KNN clustering.",
-      "Implemented user signup, profile upload, and preference inputs using React, Tailwind, and Flask.",
-      "Designed to match roommates based on vibe compatibility, similar to Tinder but for co-living.",
+      "Building a machine-learning-based roommate matching app using personality traits and KNN clustering.",
+      "Implementing user signup, profile upload, and preference inputs using React, Tailwind, and Flask.",
+      "Designing to match roommates based on vibe compatibility, similar to Tinder but for co-living.",
     ],
-    github: "https://github.com/gauravn17/vibematch",
+    github: "https://github.com/gauravn17/VibeMatch",
   },
   {
     title: "ProfoundAI",
     description: [
-      "Developed an AI-powered companion for deep reflection, emotional insight, and creative expression.",
+      "Developing an AI-powered companion for deep reflection, emotional insight, and creative expression.",
+      "Tech Stack : React for a dynamic front-end interface, Flask for a scalable back-end API and integrating HuggingFace API for natural language understanding",
     ],
     github: "https://github.com/gauravn17/ProfoundAI",
   },
   {
-    title: "Medical Billing Assistant",
+    title: "HealthHelp AI",
     description: [
-      "Built a medical billing assistant that simplifies complex billing codes using LLMs.",
-      "Integrated Streamlit for an interactive frontend and OpenAI GPT for smart text processing.",
+      "Building a medical billing assistant that simplifies complex billing codes using LLMs.",
+      "Integrated Streamlit for an interactive frontend and HuggingFace API for smart text processing.",
       "Allows users to input symptoms and receive billing code suggestions with explanations.",
     ],
-    github: "https://github.com/gauravn17/medical-billing-assistant",
+    github: "https://github.com/gauravn17/HealthHelpAI",
   },
   {
-    title: "AI Stock Summarizer",
+    title: "EquiBrief",
     description: [
       "Built a natural language summarizer for stock market news and financial reports using OpenAI’s GPT models.",
       "Parsed real-time news and company filings to generate concise, investment-focused summaries.",
       "Used LangChain and Flask to build the backend and deployed it with a React-based frontend dashboard.",
     ],
-    github: "https://github.com/gauravn17/ai-stock-summarizer",
+    github: "https://github.com/gauravn17/EquiBrief",
   },
 ]
 }
